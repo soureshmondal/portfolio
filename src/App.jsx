d902efbc30223639554a76c8636dba3cebeddfe0
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-05-07 15:49:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from 'react';
 import "./index.css";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
-import Portofolio from "./Pages/Portofolio";
-import ContactPage from "./Pages/Contact";
+import Portofolio from "./pages/Portofolio";
+import ContactPage from "./pages/Contact";
 import ProjectDetails from "./components/ProjectDetail";
-import WelcomeScreen from "./Pages/WelcomeScreen";
+import WelcomeScreen from "./pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
 
 
