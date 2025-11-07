5495480a6fdae787ab0d2aabd53941fd8cc683c6
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-11-08 04:43:45 +0530

    Update download link in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -225,7 +225,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1lxsTYHG0CMlmDfTldsiJTZkgELC9zZzD/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1oV16i6w7v1tLfVam5K-kEw6jpHa8B9sT/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
