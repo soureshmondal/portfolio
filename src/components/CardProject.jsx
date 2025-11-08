dd0621c6d727b2f0bdfbeeb30a2ebf1d17083a4b
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-11-08 22:10:26 +0530

    Update CardProject.jsx

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -23,7 +23,7 @@ const CardProject = ({ Img, Title, Description, Link: ProjectLink, github, Featu
   return (
     <div className="group relative w-full h-full flex">
             
-      <div className="relative overflow-hidden rounded-xl bg-white/10 backdrop-blur-lg border border-white/10 shadow-2xl transition-all duration-300 hover:shadow-purple-500/20 w-full h-full flex flex-col">
+      <div className="relative overflow-hidden rounded-xl bg-white/50 backdrop-blur-lg border border-white/10 shadow-2xl transition-all duration-300 hover:shadow-purple-500/20 w-full h-full flex flex-col">
         <div className="absolute inset-0 bg-gradient-to-br from-blue-500/10 via-purple-500/10 to-pink-500/10 opacity-50 group-hover:opacity-70 transition-opacity duration-300"></div>
     
         <div className="relative p-5 z-10 flex flex-col h-full ">
